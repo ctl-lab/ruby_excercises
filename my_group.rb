7bc25e3c1ff25ab2bbf5d4dfafc033052475e3e4
emiliano-ma <emilianomainero@gmail.com>
2020-08-03 08:12:42 +0200

    adds hello_world.rb and adds the 1st try of solving the challenge

--- my_group.rb
+++ my_group.rb
@@ -1,9 +1,13 @@
 my_group = []
 
-person_1 = {name: 'emi', gender: 'male', age: 30}
-person_2 = {name: 'guille', gender: 'male', age: 29}
-person_3 = {name: 'jorge', gender: 'male', age: 66}
+person_1 = {name: 'emi', gender: 'man', age: 30}
+person_2 = {name: 'guille', gender: 'man', age: 29}
+person_3 = {name: 'jorge', gender: 'man', age: 66}
 
 my_group.push  person_1
 my_group.push  person_2
 my_group.push  person_3
+
+my_group.each do |name, gender, age|
+puts "#{name} is a #{age} years old #{gender}"
+end
