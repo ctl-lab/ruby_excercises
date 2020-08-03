11c0fc117379c829ee60e820cfa63ac7d30ecf98
emiliano-ma <emilianomainero@gmail.com>
2020-08-03 23:40:55 +0200

    adds mama

--- my_group.rb
+++ my_group.rb
@@ -1,17 +1,18 @@
 my_group = []
 
-person_1 = {name: 'emi', gender: 'man', age: 30}
-person_2 = {name: 'guille', gender: 'man', age: 29}
+person_1 = {name: 'emi', gender: 'man', age: 30} 
+person_2 = {name: 'guille', gender: 'man', age: 29} 
 person_3 = {name: 'jorge', gender: 'man', age: 66}
+person_4 = {name: 'meli', gender: 'woman', age: 61} 
 
-my_group.push  person_1
 my_group.push  person_2
+my_group.push  person_1
+my_group.push  person_4
 my_group.push  person_3
+my_group.reverse
 
-# puts my_group[1][:name]
-
-my_group.each do |my_group|
-puts "#{my_group[:name].capitalize} is a #{my_group[:age]} years old #{my_group[:gender]}"
+my_group.reverse.each do |person| 
+puts "#{person[:name].capitalize} is a #{person[:age]} years old #{person[:gender]}"
 end
 
 # my_group.each do |name, gender, age|
