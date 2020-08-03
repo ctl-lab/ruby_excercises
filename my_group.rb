e7b4a5e079a836bb28ed7c6208522b06cccdc027
emiliano-ma <emilianomainero@gmail.com>
2020-08-03 07:51:11 +0200

    adds 3 people to my_group

---
+++ my_group.rb
@@ -0,0 +1,9 @@
+my_group = []
+
+person_1 = {name: 'emi', gender: 'male', age: 30}
+person_2 = {name: 'guille', gender: 'male', age: 29}
+person_3 = {name: 'jorge', gender: 'male', age: 66}
+
+my_group.push  person_1
+my_group.push  person_2
+my_group.push  person_3
